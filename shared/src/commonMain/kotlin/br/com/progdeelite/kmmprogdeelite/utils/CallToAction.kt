7768c795c2013c4643ee5d596eb6a15e296d6db5
Ricardo Ferreira <ricardo7307@gmail.com>
2023-03-17 17:56:41 +0100

    lições aprendidas kmm e como compartilhar recursos entre android e ios

--- shared/src/commonMain/kotlin/br/com/progdeelite/kmmprogdeelite/utils/CallToAction.kt
+++ shared/src/commonMain/kotlin/br/com/progdeelite/kmmprogdeelite/utils/CallToAction.kt
@@ -12,3 +12,29 @@ abstract class CallToAction {
         val number: TextResource
     ): CallToAction()
 }
+
+// 1) LICÕES APRENDIDAS - COMO COMPARTILHAR RECURSOS/
+// 2) QUAIS CLASSES EVITAR (QUE O DEV KOTLIN VAI CAIR NA ARMADILHA)
+// 3) NÃO USE DEFAULT ARGUMENTS EM CLASSES COMPARTILHADAS/WORKAROUND
+
+
+
+
+
+
+
+
+
+
+
+// NÃO FACA ASSIM! USE CLASSE ABSTRATA - IOS NÃO SABE LIDAR (AINDA) COM SEALED CLASSES
+sealed class CallToAction2 {
+    class Email(
+        val address: TextResource,
+        val subject: TextResource
+    ): CallToAction2()
+
+    class Call(
+        val number: TextResource
+    ): CallToAction2()
+}
