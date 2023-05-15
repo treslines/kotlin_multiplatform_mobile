225332b91c253ef21c987a73e2dd4449f827a3c1
Ricardo Ferreira <ricardo7307@gmail.com>
2023-05-15 15:06:03 +0200

    como estilizar textfield campo de texto em jetpack compose android studio

--- androidApp/src/main/java/br/com/progdeelite/kmmprogdeelite/android/ui/components/TextFields.kt
+++ androidApp/src/main/java/br/com/progdeelite/kmmprogdeelite/android/ui/components/TextFields.kt
@@ -3,16 +3,26 @@ package br.com.progdeelite.kmmprogdeelite.android.ui.components
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.progdeelite.kmmprogdeelite.android.ui.theme.AndroidAppTheme
 import br.com.progdeelite.kmmprogdeelite.android.ui.theme.TextStyles
 import br.com.progdeelite.kmmprogdeelite.android.utils.DependencyInjectionForPreview
@@ -30,14 +40,18 @@ import kotlinx.coroutines.flow.StateFlow
 fun DefaultTextInputField(
     initialInput: String = "",
     textFieldHint: String = "",
+    autofocus: Boolean = true,
     keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
     errorState: StateFlow<Boolean> = MutableStateFlow(false),
     errorTypeState: StateFlow<TextFieldErrorType> = MutableStateFlow(TextFieldErrorType.None),
     onInputChange: (input: String) -> Unit = {}
 ) {
     CustomTextField(
         initialInput = initialInput,
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        autofocus = autofocus,
+        keyboardType = keyboardType,
+        imeAction= imeAction,
         trailingIcon = { Icon(imageVector = Icons.Default.Error, contentDescription = null) },
         textFieldHint = textFieldHint,
         errorState = errorState,
@@ -46,11 +60,17 @@ fun DefaultTextInputField(
     )
 }
 
+// 1) COMO OBTER O FOCUS EM CAMPOS DE TEXTO
+// 2) COMO MANIPULAR O TECLADO VIRTUAL
+// 3) COMO FECHAR O TECLADO AO PRESSIONAR ENTER
+// 4) COMO ALTERAR O TIPO DO TECLADO
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-
 fun CustomTextField(
     initialInput: String = "",
-    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
+    autofocus: Boolean = false,
     trailingIcon: @Composable (() -> Unit)? = null,
     textFieldHint: String = "",
     errorState: StateFlow<Boolean>,
@@ -58,20 +78,40 @@ fun CustomTextField(
     onInputChange: (input: String) -> Unit = {}
 ) {
 
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     val error by errorState.collectAsState()
     val errorType by errorTypeState.collectAsState()
     var input by remember { mutableStateOf(TextFieldValue(initialInput)) }
 
     return Column {
 
+        if(autofocus){
+            LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+            }
+        }
+
         OutlinedTextField(
             value = input,
             singleLine = true,
             isError = error,
-            modifier = Modifier.height(Resources.Dimen.textInputField.minHeight),
+            modifier = when(autofocus) {
+                true -> Modifier
+                    .height(Resources.Dimen.textInputField.minHeight)
+                    .focusRequester(focusRequester)
+                else -> Modifier.height(Resources.Dimen.textInputField.minHeight)
+            },
             shape = RoundedCornerShape(Resources.Dimen.textInputField.roundCorner),
             trailingIcon = if (error) trailingIcon else null,
-            keyboardOptions = keyboardOptions,
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+            keyboardActions = KeyboardActions {
+                keyboardController?.hide()
+                focusManager.clearFocus()
+                onInputChange(input.text)
+            },
             onValueChange = {
                 input = it
                 onInputChange(it.text)
@@ -115,8 +155,8 @@ private fun ErrorHintText(errorType: TextFieldErrorType) {
     )
 }
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showSystemUi = true, showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true, showBackground = true)
 @Composable
 fun InputFieldsPreview() {
     DependencyInjectionForPreview()
@@ -129,9 +169,12 @@ fun InputFieldsPreview() {
     val errorNone: StateFlow<TextFieldErrorType> = MutableStateFlow(TextFieldErrorType.None)
 
     AndroidAppTheme {
-        Column {
+        Column(
+            modifier = Modifier.padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             DefaultTextInputField(
-                initialInput = "076 888 44 22",
+                initialInput = "076 888 44 222",
                 keyboardType = KeyboardType.Phone,
                 errorState = errorStateTrue,
                 errorTypeState = errorNumberNotFound
